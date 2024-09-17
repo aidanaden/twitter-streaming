cf8366e6cd09bb18f4d127f82c77f7bbe0e42b60
aidanaden <aidan@u.nus.edu>
2024-09-17 21:58:28 +0800

    chore: remove polyfill

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "@solidjs/start/config";
 import UnoCSS from "unocss/vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
   server: {
@@ -19,18 +18,18 @@ export default defineConfig({
   vite: {
     plugins: [
       UnoCSS(),
-      nodePolyfills({
-        //   // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
-        // include: ["buffer"],
-        //   // To exclude specific polyfills, add them to this list. Note: if include is provided, this has no effect
-        //   // exclude: [
-        //   //   "http", // Excludes the polyfill for `http` and `node:http`.
-        //   // ],
-        // Whether to polyfill specific globals.
-        globals: {
-          Buffer: true, // can also be 'build', 'dev', or false
-        },
-      }),
+      // nodePolyfills({
+      //   //   // To add only specific polyfills, add them here. If no option is passed, adds all polyfills
+      //   // include: ["buffer"],
+      //   //   // To exclude specific polyfills, add them to this list. Note: if include is provided, this has no effect
+      //   //   // exclude: [
+      //   //   //   "http", // Excludes the polyfill for `http` and `node:http`.
+      //   //   // ],
+      //   // Whether to polyfill specific globals.
+      //   globals: {
+      //     Buffer: true, // can also be 'build', 'dev', or false
+      //   },
+      // }),
     ],
   },
 });
