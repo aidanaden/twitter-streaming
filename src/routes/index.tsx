264fa434820eda0b646e5681efde75c893487ebc
aidanaden <aidan@u.nus.edu>
2024-09-18 08:34:55 +0800

    test: ws client open 2

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -51,13 +51,14 @@ export default function Home() {
     const client = hc("https://tweetstream-ws.adenxtreme.workers.dev");
     const ws = client.ws.$ws(0);
 
+    ws.addEventListener("open", () => {
+      ws.send("message from client");
+    });
     ws.addEventListener("message", (message) => {
       const tweet = JSON.parse(message.data) as TweetData;
       console.log("message received by server: ", { message });
       setTweets(produce((tweets) => tweets.unshift(tweet)));
     });
-
-    ws.send("open");
   }
 
   onMount(async () => {
